119581a579e363934d3a06da229265705f1ab9be
Semen Kozhin <sa.kozh@yandex.ru>
2021-12-08 13:19:55 +0100

    8.12

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Header} from "./Header/Header";
+import {Main} from "./Main/Main";
+import {Skills} from "./Skills/Skills";
+import {Projects} from "./Projects/Projects";
+import {Remote} from "./Remote/Remote";
+import {Contacts} from "./Contacts/Contacts";
+import {Footer} from "./Footer/Footer";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <Header/>
+            <Main/>
+            <Skills />
+            <Projects/>
+            <Remote />
+            <Contacts/>
+            <Footer/>
+        </div>
+    );
 }
 
 export default App;
